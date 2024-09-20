54731138541fd42059d075b70066c6873639fb98
sharma151 <sharmasaurav150503@gmail.com>
2024-09-20 15:03:33 +0545

    minorchanges

--- fetch-api/script.js
+++ fetch-api/script.js
@@ -12,7 +12,7 @@ const fetchRecipes = async (query) => {
     const recipeDiv = document.createElement("div");
     recipeDiv.classList.add("recipe");
     recipeDiv.innerHTML = `
-    <img src="${meal.strMealThumb}">
+    <img src="${meal.strMealThumb} ">
     <h3>${meal.strMeal}</h3>
     <p>${meal.strArea}</p>
     <p>${meal.strCategory}</p>
