c06c9651b82024040a2e541837dd32ed8368a4cf
sharma151 <sharmasaurav150503@gmail.com>
2024-09-20 13:52:54 +0545

    minor changes

--- fetch-api/script.js
+++ fetch-api/script.js
@@ -7,7 +7,20 @@ const fetchRecipes = async (query) => {
     `https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`
   );
   const response = await data.json();
-  console.log(response);
+
+  response.meals.forEach((meal) => {
+    const recipeDiv = document.createElement("div");
+    recipeDiv.classList.add("recipe");
+    recipeDiv.innerHTML = `
+    <img src="${meal.strMealThumb}">
+    <h3>${meal.strMeal}</h3>
+    <p>${meal.strArea}</p>
+    <p>${meal.strCategory}</p>
+    `;
+    recipeeContainer.appendChild(recipeDiv);
+  });
+
+  //   console.log(response);
 };
 
 searchBtn.addEventListener("click", (e) => {
